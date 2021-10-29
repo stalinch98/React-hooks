240e8222f9357390cb92fe770008c2e715d2de29
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-29 14:18:55 -0500

    useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
+const initialState = {
+    favorites: []
+}
+const favoriteReducer = (state, action)=>{
+    switch (action.type){
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            };
+            default:
+                return state;
+    }
+}
 const Characters = () => {
 
     const [characters, setCharacters] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character/')
@@ -10,14 +25,24 @@ const Characters = () => {
             .then(data => setCharacters(data.results))
     }, []);
 
+    const handleClick = favorite =>{
+        dispatch({type: 'ADD_TO_FAVORITE', payload: favorite})
+    }
+
     return (
         <div className="Characters">
+            {favorites.favorites.map(favorite =>(
+                <li key={favorite.id}>
+                    {favorite.name}
+                </li>
+            ))}
             {characters.map((character) => (
-                    <>
-                        <img src={character.image} alt="" />
-                        <h2>{character.name}</h2>
-                        <h1>{character.status}</h1>
-                    </>
+                <div className="item" key={character.id}>
+                    <img src={character.image} alt="" />
+                    <h2>{character.name}</h2>
+                    <h1>{character.status}</h1>
+                    <button type="button" onClick={()=> handleClick(character)}>Agregar a Favoritos</button>
+                </div>
             ))}
         </div>
     );
