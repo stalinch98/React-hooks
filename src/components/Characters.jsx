8f8b1a162a1333ea52396d007d29d96c70097b56
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-29 16:44:35 -0500

    custom hooks

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useEffect, useReducer, useMemo, useRef, useCallback } from 'react';
+import React, { useState, useReducer, useMemo, useRef, useCallback } from 'react';
 import Search from './Search';
+import useCharacters from '../hooks/useCharacters';
 
 const initialState = {
     favorites: []
 }
+
+const API = 'https://rickandmortyapi.com/api/character/';
+
 const favoriteReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TO_FAVORITE':
@@ -17,18 +21,11 @@ const favoriteReducer = (state, action) => {
 }
 const Characters = () => {
 
-    const [characters, setCharacters] = useState([]);
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
     const [search, setSearch] = useState('');
     const searchInput = useRef(null);
 
-
-
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character/')
-            .then(response => response.json())
-            .then(data => setCharacters(data.results))
-    }, []);
+    const characters = useCharacters(API);
 
     const handleClick = favorite => {
         dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
